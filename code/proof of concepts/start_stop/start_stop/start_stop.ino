845d200578ec912315221fc23adda90009dd89ba
angrybear98 <90733320+angrybear98@users.noreply.github.com>
2023-10-05 21:58:40 +0200

    Add files via upload

---
+++ code/proof of concepts/start_stop/start_stop/start_stop.ino
@@ -0,0 +1,25 @@
+const int switchPin = 2;
+const int ledPin = 13;
+volatile bool isRunning = false;
+
+void setup() {
+  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(switchPin), toggleState, FALLING);
+}
+
+void loop() {
+  if (isRunning) {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+void toggleState() {
+  isRunning = !isRunning;
+}
+
