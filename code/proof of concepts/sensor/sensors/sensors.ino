ee2470a10c45015443d0c9b483811bac00049506
angrybear98 <90733320+angrybear98@users.noreply.github.com>
2023-12-01 16:05:50 +0100

    Update sensors.ino

--- code/proof of concepts/sensor/sensors/sensors.ino
+++ code/proof of concepts/sensor/sensors/sensors.ino
@@ -1,82 +1,24 @@
-const int sensorPins[] = {A0, A1, A2, A3, A4, A5}; // Analog pins for sensors
-const int numSensors = 6;
-
-const int motorPin1 = 3;  // Right wheel
-const int motorPin2 = 5;  // Left wheel
-
-const int threshold = 80;   // Threshold for considering a black line
-const int baseSpeed = 150;  // Adjust this value based on desired speed
-const float Kp = 0.5;       // Proportional gain
-
-unsigned long lastSensorPrintTime = 0;
-const unsigned long sensorPrintInterval = 1000; // Print every 1000 milliseconds (1 second)
-
+const int sensorPin[] = {A0, A1, A2, A3, A4, A5}; 
+unsigned long previousMillis = 0;
+const long interval = 1000; 
 void setup() {
-  Serial.begin(9600); // Initialize serial communication
-  for (int i = 0; i < numSensors; ++i) {
-    pinMode(sensorPins[i], INPUT);
-  }
-  
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
+  Serial.begin(9600); 
 }
 
-void loop() {
-  int sensorValues[numSensors];
+void loop() 
+{
+  unsigned long currentMillis = millis();
 
-  // Read sensor values
-  for (int i = 0; i < numSensors; ++i) {
-    sensorValues[i] = analogRead(sensorPins[i]);
-  }
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
 
-  // Check if all sensors are below the threshold (indicating no black line)
-  if (areAllSensorsBelowThreshold(sensorValues, threshold)) {
-    // Stop the motors
-    analogWrite(motorPin1, 0);
-    analogWrite(motorPin2, 0);
+    for (int i = 0; i < 6; i++) {
+      int sensorValue = analogRead(sensorPin[i]);
 
-    // Print a message indicating no black line detected
-    Serial.println("No black line detected. Motors stopped.");
-  } else {
-    // Calculate error (difference between left and right sensors)
-    int error = sensorValues[3] - sensorValues[2];
-
-    // Adjust motor speeds based on the error using proportional control
-    int rightSpeed = baseSpeed + int(Kp * error);
-    int leftSpeed = baseSpeed - int(Kp * error);
-
-    // Make sure motor speeds are within a valid range
-    rightSpeed = constrain(rightSpeed, 0, 255);
-    leftSpeed = constrain(leftSpeed, 0, 255);
-
-    // Control the motors using the H-bridge
-    analogWrite(motorPin1, rightSpeed);
-    analogWrite(motorPin2, leftSpeed);
-  }
-
-  // Check if it's time to print sensor values
-  unsigned long currentTime = millis();
-  if (currentTime - lastSensorPrintTime >= sensorPrintInterval) {
-    // Print sensor values to Serial Monitor
-    Serial.println("Sensor Values:");
-    for (int i = 0; i < numSensors; ++i) {
       Serial.print("Sensor ");
-      Serial.print(i);
+      Serial.print(i + 1);
       Serial.print(": ");
-      Serial.println(sensorValues[i]);
-    }
-
-    // Update the last sensor print time
-    lastSensorPrintTime = currentTime;
-  }
-}
-
-bool areAllSensorsBelowThreshold(const int values[], int threshold) {
-  // Check if all sensor values are below the threshold
-  for (int i = 0; i < numSensors; ++i) {
-    if (values[i] >= threshold) {
-      return false; // At least one sensor is above or equal to the threshold
+      Serial.println(sensorValue);
     }
   }
-  return true; // All sensors are below the threshold
 }
