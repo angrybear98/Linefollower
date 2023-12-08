055651918a87ca0f01ef997c3fb385de051859dd
angrybear98 <90733320+angrybear98@users.noreply.github.com>
2023-12-08 15:50:52 +0100

    Update sensors.ino

--- code/proof of concepts/sensor/sensors/sensors.ino
+++ code/proof of concepts/sensor/sensors/sensors.ino
@@ -1,24 +1,28 @@
 const int sensorPin[] = {A0, A1, A2, A3, A4, A5}; 
 unsigned long previousMillis = 0;
 const long interval = 1000; 
+
 void setup() {
   Serial.begin(9600); 
 }
 
-void loop() 
-{
+void loop() {
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
 
+    // Create a string to store the sensor values
+    String sensorValues = "";
+
     for (int i = 0; i < 6; i++) {
       int sensorValue = analogRead(sensorPin[i]);
 
-      Serial.print("Sensor ");
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.println(sensorValue);
+      // Concatenate the sensor values to the string
+      sensorValues += "Sensor " + String(i + 1) + ": " + String(sensorValue) + "  ";
     }
+
+    // Print the sensor values on a single line
+    Serial.println(sensorValues);
   }
 }
